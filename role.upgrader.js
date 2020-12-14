d6d3f2e1cecc6ee6d84b97188725e36d38191a34
Hendrik Vosskamp <h.vosskamp@moovit.de>
2020-12-14 20:56:25 +0100

    init

---
+++ role.upgrader.js
@@ -0,0 +1,29 @@
+var roleUpgrader = {
+
+    /** @param {Creep} creep **/
+    run: function (creep) {
+
+        if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.upgrading = false;
+            creep.say('🔄 harvest');
+        }
+        if (!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+            creep.memory.upgrading = true;
+            creep.say('⚡ upgrade');
+        }
+
+        if (creep.memory.upgrading) {
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        } else {
+            if (Game.spawns.Spawn1.energy < 300) return;
+            if (creep.withdraw(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns.Spawn1);
+                creep.moveTo(Game.spawns.Spawn1, {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+    }
+};
+
+module.exports = roleUpgrader;
