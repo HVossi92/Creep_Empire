3e900eb24e9918432372031f760f9e2d71cbab6f
Hendrik Vosskamp <h.vosskamp@moovit.de>
2021-02-07 18:28:14 +0100

    Small clean up

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-const roleHarvester = require('role.harvester');
-const roleUpgrader = require('role.upgrader');
-const roleBuilder = require('role.builder');
-const roleTransporter = require('role.transporter');
 const routineSpawner = require('routine.spawner');
-const structureTower = require('structure.tower');
 const roleRunner = require('roleRunner');
 
 module.exports.loop = function () {
-
     routineSpawner.run();
     roleRunner.run();
 }
