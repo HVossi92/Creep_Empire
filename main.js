e4ba3d6078cddc50263421578bc5e60da0a26959
Hendrik Vosskamp <h.vosskamp@moovit.de>
2021-02-07 18:17:10 +0100

    removed everything unused

--- main.js
+++ main.js
@@ -7,16 +7,8 @@ const structureTower = require('structure.tower');
 
 module.exports.loop = function () {
 
-    let rooms;
-    for (let i = 0; i < Game.rooms.length; i++) {
-        rooms[i] = Game.rooms[i];
-    }
-
-    if (rooms != undefined && rooms.length > 0) {
-        const towers = rooms.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
-        towers.forEach(e => structureTower.run(e));
-    }
     routineSpawner.run();
+
 
     for (let name in Game.creeps) {
         const creep = Game.creeps[name];
